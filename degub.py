c1805527c701e13d483fd6fb1c0ca58a0a766d38
megakaos2 <merobayot@unal.edu.co>
2026-06-09 23:12:23 -0500

    Update degub.py

--- degub.py
+++ degub.py
@@ -3,3 +3,4 @@ import os
 print("SOCRATA_TOKEN:", os.getenv("SOCRATA_TOKEN") is not None)
 print("SOCRATA_USER:", os.getenv("SOCRATA_USER") is not None)
 print("SOCRATA_PASSWORD:", os.getenv("SOCRATA_PASSWORD") is not None)
+print("GOOGLE_CREDENTIALS_JSON:", os.getenv("GOOGLE_CREDENTIALS_JSON") is not None)
